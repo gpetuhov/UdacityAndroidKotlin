f317f702947e84de5d9963f1fffe3efd56065303
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-08-06 17:06:09 +0300

    Change button text

--- DiceRoller/app/src/main/java/com/gpetuhov/android/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/gpetuhov/android/diceroller/MainActivity.kt
@@ -2,11 +2,15 @@ package com.gpetuhov.android.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.text = "Let's roll"
     }
 }
