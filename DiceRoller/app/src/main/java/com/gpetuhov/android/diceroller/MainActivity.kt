12d1396756955c9a4b9dff4257531cec08007a46
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-08-06 17:13:22 +0300

    Display random numbers

--- DiceRoller/app/src/main/java/com/gpetuhov/android/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/gpetuhov/android/diceroller/MainActivity.kt
@@ -3,16 +3,25 @@ package com.gpetuhov.android.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var resultText: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
+        resultText = findViewById(R.id.result_text)
         rollButton.setOnClickListener {
-            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        val randomInt = Random.nextInt(6) + 1
+        resultText.text = randomInt.toString()
     }
 }
