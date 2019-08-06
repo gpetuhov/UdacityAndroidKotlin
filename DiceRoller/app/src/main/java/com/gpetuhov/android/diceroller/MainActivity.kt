2e73f4471a6fcef0d153ffb13a6b670d255bf3e6
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-08-06 17:07:07 +0300

    Show toast

--- DiceRoller/app/src/main/java/com/gpetuhov/android/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/gpetuhov/android/diceroller/MainActivity.kt
@@ -3,14 +3,16 @@ package com.gpetuhov.android.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.text = "Let's roll"
+        rollButton.setOnClickListener {
+            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
