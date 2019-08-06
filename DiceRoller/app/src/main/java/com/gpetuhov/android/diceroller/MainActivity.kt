3795405087dae031f41cd6213d3b7bef02311d44
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-08-06 17:27:23 +0300

    Show dice image

--- DiceRoller/app/src/main/java/com/gpetuhov/android/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/gpetuhov/android/diceroller/MainActivity.kt
@@ -3,18 +3,18 @@ package com.gpetuhov.android.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var resultText: TextView
+    private lateinit var diceImage: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
-        resultText = findViewById(R.id.result_text)
+        diceImage = findViewById(R.id.dice_image)
         rollButton.setOnClickListener {
             rollDice()
         }
@@ -22,6 +22,16 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
         val randomInt = Random.nextInt(6) + 1
-        resultText.text = randomInt.toString()
+
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
     }
 }
