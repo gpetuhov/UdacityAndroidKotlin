281fe180543d71bc2c8a21fa808de338d4409969
Gennady Petukhov <g3pmail@gmail.com>
2019-09-03 21:42:40 +0300

    Check if activity resolves

--- andfun-kotlin-android-trivia/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ andfun-kotlin-android-trivia/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -46,6 +46,12 @@ class GameWonFragment : Fragment() {
     override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
         super.onCreateOptionsMenu(menu, inflater)
         inflater?.inflate(R.menu.winner_menu, menu)
+
+        // check if the activity resolves
+        if (null == getShareIntent().resolveActivity(activity!!.packageManager)) {
+            // hide the menu item if it doesn't resolve
+            menu?.findItem(R.id.share)?.isVisible = false
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
